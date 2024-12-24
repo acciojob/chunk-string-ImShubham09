1241a246d655040752eec3e8c64f8ca792f6582d
ImShubham09 <singhashubham612@gmail.com>
2024-12-24 12:18:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function stringChop(str, size) {
   // your code here
+  if (!str) {
+    return []; // Return an empty array if the input string is null
+  }
+  
+  const result = [];
+  for (let i = 0; i < str.length; i += size) {
+    result.push(str.slice(i, i + size));
+  }
+  return result;
 }
 
-// Do not change the code below
+// Do not change the code below 
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
+
+
+// if (isNaN(size) || size <= 0) {
+//   alert("Please enter a valid positive integer for the chunk size.");
+// } else {
+//   alert(stringChop(str, size));
+// }
